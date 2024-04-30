0d21cb2acad62c48b49fff3555e05e6416f2e18f
ssong94 <kyugwang@gmail.com>
2024-04-30 22:57:03 +0900

    init

---
+++ src/test/java/com/spring/boot/jpastudy/JpaStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.boot.jpastudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
